1a53833dd85e32cde7561cacef5b186293acfd9a
Michael Kintscher <busykid1@gmail.com>
2016-02-20 14:12:01 -0700

    Initial Commit

---
+++ cse360assign3/src/cse360assign3/Calculator.java
@@ -0,0 +1,62 @@
+
+package cse360assign3;
+
+public class Calculator {
+
+	private int total;
+	
+	/**
+	 * Default constructor
+	 */
+	public Calculator () {
+		total = 0;  // not needed - included for clarity
+	}
+	
+	/**
+	 * 
+	 * @return
+	 */
+	public int getTotal () {
+		return 0;
+	}
+	
+	/**
+	 * 
+	 * @param value Integer to add
+	 */
+	public void add (int value) {
+		
+	}
+	
+	/**
+	 * 
+	 * @param value Integer to subtract
+	 */
+	public void subtract (int value) {
+		
+	}
+	
+	/**
+	 * 
+	 * @param value Integer to multiply
+	 */
+	public void multiply (int value) {
+		
+	}
+	
+	/**
+	 * 
+	 * @param value Integer to divide
+	 */
+	public void divide (int value) {
+		
+	}
+	
+	/**
+	 * 
+	 * @return
+	 */
+	public String getHistory () {
+		return "";
+	}
+}
